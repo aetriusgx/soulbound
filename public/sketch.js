fc7b61047961428459abdb4a3b2bcdc20a29477a
kovuko <jason.eh20@gmail.com>
2020-07-29 03:01:39 -0700

    Added theme switch, no event yet

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,7 @@
 let bob;
+let theme = "light";
 let toggles = {};
+toggles.theme;
 
 function preload() {
 
@@ -9,18 +11,22 @@ function setup() {
     createCanvas(windowWidth, windowHeight * .8);
     colorMode(HSB, 360, 100, 100);
     bob = new Player("Bob", [87, 65, 83, 68, 81, 69], 220);
+    
+    toggles.theme = new Clickable();
+    toggles.theme.locate(20, 20);
 }
 
 function draw() {
-    background(80);
+    if(theme == "light") background(85), toggles.theme.text = "Dark Mode", toggles.theme.textColor = "#000000";
+    if(theme == "dark") background(0), toggles.theme.text = "Light Mode", toggles.theme.textColor = "#EEEEEE";
+    
+    toggles.theme.draw();
+
     bob.draw(180, height);
 
-    toggles.theme = new Clickable();
 }
 
 function keyPressed() {
-
-    toggles.theme.locate(20, 20);
 
     bob.keyPressed();
 }
